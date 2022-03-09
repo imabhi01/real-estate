9f7ba220e3536bd7b53dce8cf9d1eebe1351bb35
imabhi01 <iamdev.abhishek@gmail.com>
2022-03-09 16:37:53 +0545

    search Filter

--- components/SearchFilter.jsx
+++ components/SearchFilter.jsx
@@ -3,11 +3,23 @@ import {useEffect, useState} from 'react'
 import {useRouter} from 'next/router'
 import {mdCancel} from 'react-icons/md'
 import Image from 'next/image'
+import { filterProps } from 'framer-motion'
+import { filterData, getFilterValues} from '../utils/filterData'
+import { baseUrl, fetchApi } from '../utils/fetchApi'
 
-const SearchFilter = () => (
-    <Flex bg="gray.100" justifyContent="center" flexWrap="wrap">
-        
-    </Flex>
-);
+export default function SearchFilters(){
+    const [filters, setfilters] = useState(filterData);
+    console.log(filters);
+    return (
+        <Flex bg="gray.100" justifyContent="center" alignItems="center">
+            {filters.map((filterItem) => (
+                <Box p="4" key={filterItem.queryName}>
+                    <Select bg="white.200">
+                        <option value="">{filterItem.placeholder}</option>
+                    </Select>
+                </Box>
+            ))}
+        </Flex>
+    )
 
-export default SearchFilter;
+}
